96a80db08dd5d9b974b64035e6bfc1b7097571ad
Pierre <pierre.lagadec@gmail.com>
2022-01-19 12:02:33 +1100

    Deno rest api - adding routes middleware to the app

--- deno/app.ts
+++ deno/app.ts
@@ -1,9 +1,14 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
+import todoRoutes from './routes/todos.ts';
+
 const app = new Application();
 
 // app.use((ctx) => {
 //   ctx.response.body = "Hello World!";
 // });
+
+app.use(todoRoutes.routes());
+app.use(todoRoutes.allowedMethods());
 
 await app.listen({ port: 3000 });

fde7c39b1e6471a7d00535e6f7f2276bc91eebe6
Pierre <pierre.lagadec@gmail.com>
2022-01-19 13:10:49 +1100

    Deno - handling CORS errors

--- deno/app.ts
+++ deno/app.ts
@@ -5,9 +5,16 @@ import todosRoutes from './routes/todos.ts';
 const app = new Application();
 
 app.use(async (ctx, next) => {
-  console.log('Middleware!');
+  console.log('middleware!');
   await next();
 });
+
+app.use(async (ctx, next) => {
+  ctx.response.headers.set('Access-Control-Allow-Origin', '*'); //allow every domain to send requests
+  ctx.response.headers.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); //allows methods
+  ctx.response.headers.set('Access-Control-Allow-Headers', 'Content-Type'); //alows content-type header to be set
+  await next();
+})
 
 app.use(todosRoutes.routes());
 app.use(todosRoutes.allowedMethods());

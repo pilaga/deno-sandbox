82acbdba5f43407c2f4a369bd7bdefccd03b4c4a
Pierre <pierre.lagadec@gmail.com>
2022-01-19 13:29:47 +1100

    Deno - connecting Deno to MongoDB

--- deno/app.ts
+++ deno/app.ts
@@ -1,6 +1,9 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
+import { connect } from './helpers/db_client.ts';
 import todosRoutes from './routes/todos.ts';
+
+connect();
 
 const app = new Application();
 
---
+++ deno/helpers/db_client.ts
@@ -0,0 +1,13 @@
+import { MongoClient, Database } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
+
+let db: Database;
+
+export function connect() {
+    const client = new MongoClient();
+    client.connectWithUri("mongodb+srv://admin:<password>@cluster0.lrvxm.mongodb.net/denodb?retryWrites=true&w=majority");
+    db = client.database('todo-db');
+}
+
+export function getDb() {
+    return db;
+}

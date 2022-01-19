fe854e8abfb9ac73d923e296af024bfb63072ba1
Pierre <pierre.lagadec@gmail.com>
2022-01-19 13:37:45 +1100

    Deno - fetch from database

--- deno/routes/todos.ts
+++ deno/routes/todos.ts
@@ -1,4 +1,6 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
+import { ObjectId } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
+import { getDb } from '../helpers/db_client';
 
 const router = new Router();
 
@@ -10,35 +12,42 @@ interface Todo {
 let todos: Todo[] = [];
 
 router.get('/todos', (ctx) => {
-  ctx.response.body = { todos: todos };
+    const todos = await getDb().collection('todos').find();
+    const transTodo = todos.map((todo: { _id: ObjectId, text: string }) => {
+        return {
+            id: todo._id.$oid,
+            text: todo.text
+        }
+    })
+    ctx.response.body = { todos: transTodo };
 });
 
 router.post('/todos', async (ctx) => {
-  const data = await ctx.request.body();
-  const newTodo: Todo = {
-    id: new Date().toISOString(),
-    text: data.value.text,
-  };
+    const data = await ctx.request.body();
+    const newTodo: Todo = {
+        id: new Date().toISOString(),
+        text: data.value.text,
+    };
 
-  todos.push(newTodo);
+    todos.push(newTodo);
 
-  ctx.response.body = { message: 'Created todo!', todo: newTodo };
+    ctx.response.body = { message: 'Created todo!', todo: newTodo };
 });
 
 router.put('/todos/:todoId', async (ctx) => {
-  const tid = ctx.params.todoId;
-  const data = await ctx.request.body();
-  const todoIndex = todos.findIndex((todo) => {
-    return todo.id === tid;
-  });
-  todos[todoIndex] = { id: todos[todoIndex].id, text: data.value.text };
-  ctx.response.body = { message: 'Updated todo' };
+    const tid = ctx.params.todoId;
+    const data = await ctx.request.body();
+    const todoIndex = todos.findIndex((todo) => {
+        return todo.id === tid;
+    });
+    todos[todoIndex] = { id: todos[todoIndex].id, text: data.value.text };
+    ctx.response.body = { message: 'Updated todo' };
 });
 
 router.delete('/todos/:todoId', (ctx) => {
-  const tid = ctx.params.todoId;
-  todos = todos.filter((todo) => todo.id !== tid);
-  ctx.response.body = { message: 'Deleted todo' };
+    const tid = ctx.params.todoId;
+    todos = todos.filter((todo) => todo.id !== tid);
+    ctx.response.body = { message: 'Deleted todo' };
 });
 
 export default router;
